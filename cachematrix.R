9068b457673bb20bb75ce3cc158416e5f033fe03
Marco Antonio Andrade Barrera <mandradebs@gmail.com>
2014-09-21 17:01:18 -0500

    I added some descriptions to undertand how works every function

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The firt function allow to create and special matrix object
+## that can cache its inverse, the second functions can verified if
+## the inverse of a "special matrix" has been calcutated and print out
+## the inverse. If the inverse has not been calculated, then this function
+## calculate and print the inverse of the matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache 
+## its inverse.
+## This function add new features to a matrix obsjet. This features are three
+## functions ("get" that return the matrix data, "setinverse" that set the
+## inverse of the matrix, and "getinverse" that return the inverse) and 
+## one attribute (the inverse of the matix).
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -18,8 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special 
+## "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve 
+## retrieve the inverse from the cache, else the inverse is calculated
+## and the result is set to the special matrix features (via setinverse)
+## and then is printed out.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
